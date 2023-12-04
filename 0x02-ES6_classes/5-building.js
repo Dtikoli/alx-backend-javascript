425e0e96efb1acb3764ee10df960ce0468d4440b
Dtikoli <tikolidavid@yahoo.com>
2023-12-04 05:56:42 -0800

    Updating solution 5

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,13 +1,7 @@
-/* eslint-disable no-underscore-dangle */
 class Building {
   constructor(sqft) {
-    if (
-      this.constructor !== Building
-      && typeof this.evacuationWarningMessage !== 'function'
-    ) {
-      throw Error(
-        'Class extending Building must override evacuationWarningMessage',
-      );
+    if (new.target !== Building && typeof this.evacuationWarningMessage !== 'function') {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
     }
 
     this._sqft = sqft;
