6789ad06930339171a95da1261796151515b4856
Dtikoli <tikolidavid@yahoo.com>
2023-12-15 13:18:47 -0800

    Adding references to Namespace & Declaration merging

--- 0x04-TypeScript/task_4/js/main.ts
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -1,3 +1,8 @@
+/// <reference path="./subjects/Subject.ts" />
+/// <reference path="./subjects/Cpp.ts" />
+/// <reference path=".subjects/Java.ts" />
+/// <reference path="./subjects/React.ts" />
+
 export const cpp: Subjects.Cpp = new Subjects.Cpp();
 export const java: Subjects.Java = new Subjects.Java();
 export const react: Subjects.React = new Subjects.React();
